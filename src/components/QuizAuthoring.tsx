889dc3a41cd1498e222a113febcdf445a3629446
yu-mathy14 <yuna14.navi1031@gmail.com>
2026-07-31 04:57:06 +0000

    Make Question Creation Mode

---
+++ src/components/QuizAuthoring.tsx
@@ -0,0 +1,31 @@
+import QuestionForm from "./QuestionForm";
+import QuestionList from "./QuestionList";
+import type { Question } from "../types";
+
+/* 【型エイリアス】型定義 */
+type QuizAuthoringProps = {
+  /* Question型のオブジェクトが入った配列 */
+  questions: Question[];
+
+  /* Question型を受け取って何も返さない関数 */
+  onAdd: (question: Question) => void;
+};
+
+/* Reactコンポーネントを作るための関数 */
+export default function QuizAuthoring({
+  /* QuizAuthoringProps型のオブジェクト */
+  questions,
+  onAdd,
+}: QuizAuthoringProps) {
+
+  /* 画面の表示内容 */
+  return (
+    <>
+      {/* QuestionFormへProps(値)を渡している */}
+      <QuestionForm onAdd={onAdd} />
+
+      {/* QuestionListへProps(値)を渡している */}
+      <QuestionList questions={questions} />
+    </>
+  );
+}
